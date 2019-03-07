efaff17608f79960fa59197de8610d13a5cbc0b0
Hippohaha7 <lwang725@gmail.com>
2019-03-07 01:01:08 -0500

    Added the CS61B Skeleton and Lecture folders

---
+++ lectureCode-sp18/exercises/lists1/Lists1Exercises.java
@@ -0,0 +1,44 @@
+public class Lists1Exercises {
+    /** Returns an IntList identical to L, but with
+      * each element incremented by x. L is not allowed
+      * to change. */
+    public static IntList incrList(IntList L, int x) {
+        IntList Temp = new IntList(0,null);
+        IntList P = Temp;
+        while(L.rest!=null){
+            Temp.first = L.first + x;
+            Temp.rest = new IntList(L.rest.first+x, null);
+            Temp = Temp.rest;
+            L= L.rest;
+        }
+
+        return P;
+    }
+
+    /** Returns an IntList identical to L, but with
+      * each element incremented by x. Not allowed to use
+      * the 'new' keyword. */
+    public static IntList dincrList(IntList L, int x) {
+        while (L!= null){
+            L.first += x;
+            L = L.rest;
+        }
+        return L;
+    }
+
+    public static void main(String[] args) {
+        IntList L = new IntList(5, null);
+        L.rest = new IntList(7, null);
+        L.rest.rest = new IntList(9, null);
+
+        //System.out.println(L.size());
+        //System.out.println(L.iterativeSize());
+
+        // Test your answers by uncommenting. Or copy and paste the
+        // code for incrList and dincrList into IntList.java and
+        // run it in the visualizer.
+        //System.out.println(L.get(1));
+        System.out.println(incrList(L, 3));
+        //System.out.println(dincrList(L, 3));
+    }
+}
